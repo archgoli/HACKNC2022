d2b07782b7a7714e51fd7b94b5ac07c2f7cd8b16
Archana Goli <archgoli@unc.edu>
2022-11-07 17:58:35 -0500

    finished v0

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
+
+
+
+request_url = "https://hawaiibeachsafety.com/rest/conditions.json"
+response = requests.get(request_url)
+
 
 app = Flask(__name__)
+
+def find_index(select_beach: str):
+    list_of_beaches: list[str] = []
+    for beach in response.json():
+        list_of_beaches.append(beach['beach'])
+    index_of_beach: int = 0
+    while index_of_beach < len(list_of_beaches):
+        beach = list_of_beaches[index_of_beach]
+        if beach != select_beach:
+            index_of_beach += 1
+        else:
+            return index_of_beach
 
 @app.route('/')
 def home():
     return render_template('home.html')
-
-@app.route('/about')
-def about():
-    return render_template("about.html")
 
 @app.route('/ourmission')
 def ourmission():
@@ -20,4 +35,21 @@ def process():
 
 @app.route('/demo')
 def demo():
-    return render_template("demo.html")
+    list_of_beaches: list[str] = []
+    for beach in response.json():
+        list_of_beaches.append(beach['beach'])
+    return render_template("demo.html", list_of_beaches=list_of_beaches)
+
+@app.route('/result', methods=['GET', 'POST'])
+def result():
+    select_beach = request.form.get('comp_select')
+    index_of_beach: int = find_index(select_beach)
+    full_dictionary_of_data = response.json()[index_of_beach]
+    return render_template("result.html", data=full_dictionary_of_data)
+
+
+
+
+
+
+
